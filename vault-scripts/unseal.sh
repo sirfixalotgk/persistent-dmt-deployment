5346cc9536fcbec9cb3f557a1dbb7335a1f73751
sirfixalotgk <sirfixalot@woh.rr.com>
2025-03-21 10:55:54 +0000

    Updated sleep timer in unseal script. 21MAR25

--- vault-scripts/unseal.sh
+++ vault-scripts/unseal.sh
@@ -5,14 +5,11 @@
 # Second script in forked launch of container.  This script is called by launch.sh    #
 # and runs in parallel to the vault process.                                          #
 #                                                                                     #
-# CAUTION: Consider passing un-seal key as a parameter to avoid embedding in the      #
-#          URL below                                                                  #
-#                                                                                     #
 #######################################################################################
 key=$VAULT_KEY
 token=$VAULT_DEV_ROOT_TOKEN_ID
 echo "Waiting for the Vault container to be ready to accept connections..."
-sleep 10
+sleep 5
 echo "Calling un-seal API..."
 wget --spider --header 'accept: application/json' --header 'Content-Type: application/json' --header "X-Vault-Token: $token" --post-data '{"key": "'$key'", "migrate": false, "reset": false}' 'http://127.0.0.1:8200/v1/sys/unseal'
 sleep 1
