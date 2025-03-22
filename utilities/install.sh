0840588e794458d55a5d69a728baabea1163bc0d
sirfixalotgk <sirfixalot@woh.rr.com>
2025-03-22 20:29:40 +0000

    Install script updates - CLI switches. 22MAR25

--- utilities/install.sh
+++ utilities/install.sh
@@ -40,4 +40,4 @@ git clone https://github.com/device-management-toolkit/rpc-go.git
 rm -f ./postgres-data/.commit
 rm -f ./vault-pd/.commit
 echo "\n\nCalling PowerShell install,init and configuration script...enjoy!"
-pwsh -ExecutionPolicy Bypass -Command "./utilities/install.ps1 -mpsCN `"$mpsCN`" -webUiPass `"$webUiPass`" -dbPass `"$dbPass`""
+pwsh -ExecutionPolicy Bypass -Command "./utilities/install.ps1 -mpsCN \"$mpsCN\" -webUiPass \"$webUiPass\" -dbPass \"$dbPass\""
