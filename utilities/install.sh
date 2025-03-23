56d7aec0e7808c327a820abeb3ac66c70148351f
sirfixalotgk <sirfixalot@woh.rr.com>
2025-03-23 09:19:06 +0000

    Install script updates - CLI switches. 23MAR25

--- utilities/install.sh
+++ utilities/install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#
-#
 #############################################################################################################
 #                                                                                                           #
 #  Device Management ToolKit installation and configuration script...................v0.1.0 21MAR25         #
@@ -14,34 +12,58 @@
 #     OS platforms (future).                                                                                #
 #                                                                                                           #
 #############################################################################################################
-mpsCN=$1
-webUiPass=$2
-dbPass=$3
-echo "Updating APT and installing pre-reqs..."
-apt update
+read -p "Please enter the FQDN to be used for this deployment: " mpsCN
+read -s -p "Please enter the password for the WebUI \"admin\" user: " webUiPass
+if [ -n "$webUiPass" ]; then
+  echo "Password saved."
+else
+  echo "You must supply a password for the admin account."
+  read -s -p "Please enter the password for the WebUI \"admin\" user: " webUiPass
+  if [ -z "$webUiPass" ]; then
+    exit
+  fi
+fi
+read -s -p "Please enter the database connection password: " dbPass
+if [ -n "$dbPass" ]; then
+  echo "Password saved."
+else
+  echo "You must supply a password for the database connection."
+  read -s -p "Please enter the database connection password: " dbPass
+  if [ -z "$dbPass" ]; then
+    exit
+  fi
+fi
+echo "\n\nUpdating APT and installing pre-reqs..."
+apt update >> /dev/null
 apt install docker-buildx docker-clean docker-compose-v2 docker-compose docker-doc docker-registry docker.io python3-docker python3-dockerpty -y
-sleep 5
-echo "\n\nInstalling PowerShell v7.5.0..."
-dpkg -i ./utilities/powershell_7.5.0-1.deb_amd64.deb
-echo "\n\nResolving any dependency orphans..."
-apt install -f -y
-sleep 5
+echo "\n\nChecking to see if we need PowerShell..."
+checkPwsh=$(apt list | grep powershell)
+if [[ $check == *"powershell"* ]] && [[ $check == *"installed"* ]]; then
+  echo "\n\nAPT reports PowerShell is installed.  If you experience issues, package management may require cleanup."
+else
+  echo "\n\nInstalling PowerShell v7.5.0..."
+  dpkg -i ./utilities/powershell_7.5.0-1.deb_amd64.deb
+  echo "\n\nResolving any dependency orphans..."
+  apt install -f -y >> /dev/null
+  sleep 1
+fi
 echo "\n\nCloning submodules from Intel(R) AMT Device Management Toolkit repository..."
-git clone https://github.com/device-management-toolkit/rps.git
-git clone https://github.com/device-management-toolkit/mps.git
-git clone https://github.com/device-management-toolkit/mps-router.git
-git clone https://github.com/device-management-toolkit/ui-toolkit.git
-git clone https://github.com/device-management-toolkit/sample-web-ui.git
-git clone https://github.com/device-management-toolkit/ui-toolkit-react.git
-git clone https://github.com/device-management-toolkit/ui-toolkit-angular.git
-git clone https://github.com/device-management-toolkit/rpc.git
-git clone https://github.com/device-management-toolkit/rpc-go.git
+git clone https://github.com/device-management-toolkit/rps.git >> /dev/null
+echo " -- RPS cloned..."
+git clone https://github.com/device-management-toolkit/mps.git >> /dev/null
+echo " -- MPS cloned..."
+git clone https://github.com/device-management-toolkit/mps-router.git >> /dev/null
+echo " -- MPS Router cloned..."
+git clone https://github.com/device-management-toolkit/ui-toolkit.git >> /dev/null
+git clone https://github.com/device-management-toolkit/sample-web-ui.git >> /dev/null
+git clone https://github.com/device-management-toolkit/ui-toolkit-react.git >> /dev/null
+git clone https://github.com/device-management-toolkit/ui-toolkit-angular.git >> /dev/null
+echo " -- UI samples and integration kits cloned..."
+git clone https://github.com/device-management-toolkit/rpc.git >> /dev/null
+git clone https://github.com/device-management-toolkit/rpc-go.git >> /dev/null
+echo " -- RPC components cloned..."
 # Cleaning GIT orphans that can cause issues with services that expect empty data directories
 rm -f ./postgres-data/.commit
 rm -f ./vault-pd/.commit
 echo "\n\nCalling PowerShell install,init and configuration script...enjoy!"
-if [ -n "$mpsCN" ] -a [ -n "$webUiPass" ] -a [ -n "$dbPass"]; then
-  pwsh -ExecutionPolicy Bypass -Command "./utilities/install.ps1 -mpsCN \"$mpsCN\" -webUiPass \"$webUiPass\" -dbPass \"$dbPass\""
-else
-  pwsh -ExecutionPolicy Bypass -Command "./utilities/install.ps1"
-fi
+pwsh -ExecutionPolicy Bypass -Command "./utilities/install.ps1 -mpsCN \"$mpsCN\" -webUiPass \"$webUiPass\" -dbPass \"$dbPass\""
