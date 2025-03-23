9fd8dd2342029666ea52a1842da2545ee8f330cb
sirfixalotgk <sirfixalot@woh.rr.com>
2025-03-23 11:32:20 +0000

    Release installation script and removal script. 23MAR25

--- utilities/install.sh
+++ utilities/install.sh
@@ -13,51 +13,51 @@
 #                                                                                                           #
 #############################################################################################################
 tput clear
-echo "Welcome to the Intel(R) AMT Device Management Toolkit - Cloud Deployment automated installer script."
-echo -e "\nYou need to supply some minimal information but then we'll take it from there...Thank you and ENJOY!!!!\n\n"
+echo -e "\e[35mWelcome to the Intel(R) AMT Device Management Toolkit - Cloud Deployment automated installer script.\e[0m"
+echo -e "\n\e[34mYou need to supply some minimal information but then we'll take it from there...Thank you and ENJOY!!!! \e[0m\n\n"
 read -p "Please enter the FQDN to be used for this deployment: " mpsCN
-echo -e "\n\n\"$mpsCN\" will be used for this deployment.\n\n"
+echo -e "\n\n\e[32m\"$mpsCN\" will be used for this deployment. \e[0m\n\n"
 read -ers -p "Please enter the password for the WebUI \"admin\" user: " webUiPass
 if [ -n "$webUiPass" ]; then
-  echo -e "Password saved.\n"
+  echo -e "\e[32mPassword saved. \e[0m\n"
 else
-  echo "You must supply a password for the admin account."
+  echo -e "\e[33mYou must supply a password for the admin account. \e[0m"
   read -ers -p "Please enter the password for the WebUI \"admin\" user: " webUiPass
   if [ -z "$webUiPass" ]; then
-    echo -e "\n\n****Password requirement not satisfied...exiting!****"
+    echo -e "\n\n\e[31m****Password requirement not satisfied...exiting!**** \e[0m"
     exit
   else
-    echo -e "Password saved.\n"
+    echo -e "\e[32mPassword saved. \e[0m\n"
   fi
 fi
 read -ers -p "Please enter the database connection password: " dbPass
 if [ -n "$dbPass" ]; then
-  echo -e "Password saved.\n"
+  echo -e "\e[32mPassword saved. \e[0m\n"
 else
-  echo "You must supply a password for the database connection."
+  echo "\e[33mYou must supply a password for the database connection. \e[0m"
   read -ers -p "Please enter the database connection password: " dbPass
   if [ -z "$dbPass" ]; then
-    echo -e "\n\n****Password requirement not satisfied...exiting!****"
+    echo -e "\n\n\e[31m****Password requirement not satisfied...exiting!**** \e[0m"
     exit
   else
-    echo -e "Password saved.\n"
+    echo -e "\e[32mPassword saved. \e[0m\n"
   fi
 fi
-echo -e "\n\nUpdating APT and installing pre-reqs..."
+echo -e "\n\n\e[32mUpdating APT and installing pre-reqs..."
 apt update >> /dev/null
 apt install docker-buildx docker-clean docker-compose-v2 docker-compose docker-doc docker-registry docker.io python3-docker python3-dockerpty -y
 echo -e "\n\nChecking to see if we need PowerShell..."
 checkPwsh=$(apt list | grep powershell)
-if [[ "$check" == *"powershell"* ]] && [[ "$check" == *"installed"* ]]; then
+if [[ "$checkPwsh" == *"powershell"* ]] && [[ "$checkPwsh" == *"installed"* ]]; then
   echo -e "\n\nAPT reports PowerShell is installed.  If you experience issues, package management may require cleanup."
 else
-  echo -e "\n\nInstalling PowerShell v7.5.0..."
+  echo -e "\n\n\e[33mInstalling PowerShell v7.5.0..."
   dpkg -i ./utilities/powershell_7.5.0-1.deb_amd64.deb
-  echo -e "\n\nResolving any dependency orphans..."
+  echo -e "\n\nResolving any dependency orphans... \e[0m"
   apt install -f -y >> /dev/null
   sleep 1
 fi
-echo -e "\n\nCloning submodules from Intel(R) AMT Device Management Toolkit repository..."
+echo -e "\n\n\e[32mCloning submodules from Intel(R) AMT Device Management Toolkit repository..."
 git clone https://github.com/device-management-toolkit/rps.git >> /dev/null
 echo " -- RPS cloned..."
 git clone https://github.com/device-management-toolkit/mps.git >> /dev/null
@@ -75,5 +75,5 @@ echo " -- RPC components cloned..."
 # Cleaning GIT orphans that can cause issues with services that expect empty data directories
 rm -f ./postgres-data/.commit
 rm -f ./vault-pd/.commit
-echo -e "\n\nCalling PowerShell install,init and configuration script...enjoy!"
+echo -e "\n\n\e[36mCalling PowerShell install,init and configuration script...enjoy! \e[0m"
 pwsh -ExecutionPolicy Bypass -Command "./utilities/install.ps1 -mpsCN \"$mpsCN\" -webUiPass \"$webUiPass\" -dbPass \"$dbPass\""
