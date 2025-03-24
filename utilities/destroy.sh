2975a2bfbedd7516bf1a7f41117388841d58e037
sirfixalotgk <sirfixalot@woh.rr.com>
2025-03-24 09:32:28 +0000

    Release installation and removal script as well as welcome/disclaimer message(s). 24MAR25

--- utilities/destroy.sh
+++ utilities/destroy.sh
@@ -4,19 +4,18 @@
 #    NO DATA IS PRESERVED!!!                                               #
 ############################################################################
 tput clear
-echo -e "\n\nDESTROY this deployment of the Intel(R) AMT Device Management Toolkit?\n"
+echo -e "\n\n\e[31mDESTROY this deployment of the Intel(R) AMT Device Management Toolkit?\n\e[33m"
 read -er -p "Type 'destroy' to obliterate this deployment: " destroyConfirm
 if [[ "$destroyConfirm" == "destroy" ]]; then
-  echo -e "\nTaking containers offline..."
+  echo -e "\n\e[35mTaking containers offline..."
   docker-compose down -v
-  echo -e "\nPruning everything..."
-  docker image prune -a -f
-  docker volume prune -a -f
-  docker container prune -f
+  echo -e "\nPruning everything (cache, images, containers...everything)..."
+  docker system prune -a -f
   sleep 1
   echo -e "\n\nRemoving all files including dot-hidden files..."
   rm -fR *
   rm -fR .*
+  echo -e "\n\n\e[0mOkay, it's all gone!"
 else
-  echo -e "\nWhew!  That was close!  But, if you did mean to discard everything, 'destroy' (lowercase) must be entered to confirm."
+  echo -e "\n\e[0mWhew!  That was close!  But, if you did mean to discard everything, 'destroy' (lowercase) must be entered to confirm."
 fi
